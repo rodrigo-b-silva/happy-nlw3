f0a9cfd64fd5a22225c45f4c131c2d368554eb6d
rodrigo.b.silva <rodrigo.barbosa@fcamara.com.br>
2020-10-18 11:49:35 -0300

    Correção app - remoção mobile

---
+++ app/src/pages/OrphanageDetail.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export default function OrphanageDetail() {
+  return <Text>Teste</Text>
+}
---
+++ app/src/routes.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const { Navigator, Screen } = createStackNavigator();
+
+import OrphanagesMap from './pages/OrphanagesMap';
+import OrphanageDetail from './pages/OrphanageDetail';
+
+export default function Router() {
+  return (
+    <NavigationContainer>
+      <Navigator screenOptions={{ headerShown: false }}>
+        <Screen name="OrphanagesMap" component={OrphanagesMap} />
+        <Screen name="OrphanageDetail" component={OrphanageDetail} options={{ headerShown: true }} />
+      </Navigator>
+    </NavigationContainer>
+  )
+}
